fed660fead9bbec04660cb68214caf01cd347154
Benjamin Arkorful <benprimeartist@gmail.com>
2026-01-26 11:32:04 +0000

    edited app.jsx to include Browser router

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,11 +1,11 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <h1>Recipe Sharing App</h1>
 
       <AddRecipeForm />
@@ -14,11 +14,12 @@ function App() {
         <Route path="/" element={<RecipeList />} />
         <Route path="/recipe/:id" element={<RecipeDetails />} />
       </Routes>
-    </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
 
 
 
