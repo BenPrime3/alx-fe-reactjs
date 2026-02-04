c011e8bc9876c96c7e1843e96bf5eea1fb7e0842
Benjamin Arkorful <benprimeartist@gmail.com>
2026-02-04 12:32:11 +0000

    Fix incorrect className

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ function UserProfile() {
   return (
     <div className="user-profile bg-gray-100 sm:p-4 md:p-8 p-8 sm:max-w-xs md:max-w-sm max-w-sm mx-auto,my-20 rounded-lg shadow-lg">
 
-      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-36 h-36 mx-auto sm:w-s4 sm:h-s4 md:w-36 md:h-36" />
+      <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36" />
 
       <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl">John Doe</h1>
 
