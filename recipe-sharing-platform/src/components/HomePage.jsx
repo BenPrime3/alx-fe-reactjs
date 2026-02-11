4059bcd7afe244dc7d2b1a786f3c726c0fe269ae
Benjamin Arkorful <benprimeartist@gmail.com>
2026-02-11 11:50:47 +0000

    Add RecipeDetail Component and add tailwind css

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-import data from "../data.json"
+import { Link } from "react-router-dom";
+import data from "../data.json";
 
 function HomePage() {
 
@@ -14,13 +15,19 @@ function HomePage() {
       <h1 className="text-center text-xl font-extrabold my-9 mb-20">Recipe Collection</h1>
 
       <div className="grid gap-16 m-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-items-center">
+
         {recipes.map((recipe) => (
-          <div key={recipe.id} className="bg-slate-400 w-52 p-6 rounded-lg hover:shadow-xl hover:scale-105 2xl:transition duration-500">
+          <Link key={recipe.id} to={`/recipe/${recipe.id}`}>
+
+            <div className="bg-slate-400 w-52 p-6 rounded-lg hover:shadow-xl hover:scale-105 2xl:transition duration-500">
             <img src={recipe.image} alt="RecipePic" />
             <h3 className="font-bold">{recipe.title}</h3>
             <p>{recipe.summary}</p>
           </div>
+
+          </Link>
         ))}
+
       </div>
     </div>
   )
---
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -0,0 +1,34 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import data from "../data.json";
+
+function RecipeDetail() {
+  const { id } = useParams(); 
+  const [recipe, setRecipe] = useState(null);
+
+  useEffect(() => {
+    const foundRecipe = data.find((r) => r.id === Number(id));
+    setRecipe(foundRecipe);
+  }, [id]);
+
+  if (!recipe) return <p>Loading recipe...</p>;
+
+  return (
+    <div className="flex flex-col justify-center justify-self-center mt-10 text-center">
+      <h1>{recipe.name}</h1>
+      <img src={recipe.image} alt={recipe.title} />
+
+      <h2 className="font-bold text-xl p-6 pb-2">Ingredients</h2>
+      <ul>
+        {recipe.ingredients.map((item, i) => (
+          <li key={i}>{item}</li>
+        ))}
+      </ul>
+
+      <h2 className="font-bold mt-6 mb-2">Instructions</h2>
+      <p>{recipe.instructions}</p>
+    </div>
+  );
+}
+
+export default RecipeDetail;
