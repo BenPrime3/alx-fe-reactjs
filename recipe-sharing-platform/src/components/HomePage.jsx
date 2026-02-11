8ac3c4522bb60a78aab016abe4a59249b8270a5a
Benjamin Arkorful <benprimeartist@gmail.com>
2026-02-11 09:44:54 +0000

    Add tailwind utility classes

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -11,16 +11,16 @@ function HomePage() {
 
   return (
     <div>
-      <h1>Recipe Collection</h1>
+      <h1 className="text-center text-xl font-extrabold my-9 mb-20">Recipe Collection</h1>
 
-      <div>
+      <div className="grid gap-16 m-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-items-center">
         {recipes.map((recipe) => (
-          <div key={recipe.id}>
+          <div key={recipe.id} className="bg-slate-400 w-52 p-6 rounded-lg hover:shadow-xl hover:scale-105 2xl:transition duration-500">
             <img src={recipe.image} alt="RecipePic" />
-            <h3>{recipe.title}</h3>
+            <h3 className="font-bold">{recipe.title}</h3>
             <p>{recipe.summary}</p>
           </div>
-        ))}...
+        ))}
       </div>
     </div>
   )
