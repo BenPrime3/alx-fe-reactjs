38eafe12d66d096ab3f0a4a5bf0c431c4452e2a7
Benjamin Arkorful <benprimeartist@gmail.com>
2026-02-24 11:14:52 +0000

    Setup React router and implement nested and dynamic routes

---
+++ react-router-advanced/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link to="/profile">Go to Profile</Link>
+      <br />
+      <Link to="/blog/1">Go to Blog Post 1</Link>
+    </div>
+  );
+}
+
+export default Home;
