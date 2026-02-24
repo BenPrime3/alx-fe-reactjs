461ea9ce8eaeb546a5c16f191138ed82d8c1348f
Benjamin Arkorful <benprimeartist@gmail.com>
2026-02-24 11:21:55 +0000

    Refactor code

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,44 +1,28 @@
-import {BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
-import Profile from "./components/Profile";
-import ProfileDetails from "./components/ProfileDetails";
-import ProfileSettings from "./components/ProfileSettings";
 import BlogPost from "./components/BlogPost";
+import Profile from "./components/Profile";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
 
-          
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+      <Route
+        path="/profile/*"
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
 
-          
-          <Route path="/profile"
-            element={
-              <ProtectedRoute>
-                <Profile />
-              </ProtectedRoute>
-            }
-          >
-            <Route path="details" element={<ProfileDetails />} />
-            <Route path="settings" element={<ProfileSettings />} />
-          </Route>
-
-          
-          <Route path="/blog/:id" element={<BlogPost />} />
-
+      <Route path="/blog/:id" element={<BlogPost />} />
     </Routes>
   );
-
-      </BrowserRouter>
-    </>
-  )
 }
 
-export default App
+export default App;
